d34b9a7886a8b0fe9fca08147c950f086048b2bd
EchoFD54 <aaronfd54@gmail.com>
2026-09-18 13:25:45 +0200

    Added dice physics
    dice can now be rolled using the space bar key, it will make an arc motion in a random direction with random torque and force, and it will check if the dice has stopped rolling.
    
    Only issue is that it is a bit floaty so it rolls like its made of paper, but im not sure how to fix this yet

--- Assets/Scripts/DiceController.cs
+++ Assets/Scripts/DiceController.cs
@@ -1,16 +1,60 @@
+using System;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class DiceController : MonoBehaviour{
-    void Start(){
-        
+    [SerializeField] 
+    private float minRollForce = 10f;
+    [SerializeField]
+    private float maxRollForce = 20f;
+    [SerializeField]
+    private float minRollTorque = 10f;
+    [SerializeField]
+    private float maxRollTorque = 20f;
+
+    private Rigidbody rb;
+    private float rollTimer = 0f;
+    public bool isRolling = false;
+
+    void Awake(){
+        rb = GetComponent<Rigidbody>();
     }
 
-    void fixedUpdate(){
-        
+    void FixedUpdate(){
+        if(isRolling){
+            rollTimer += Time.fixedDeltaTime;
+            //wait here a couple seconds before checking if the dice has stopped rolling
+            if(rollTimer > 3f){
+                if(rb.linearVelocity.magnitude < 0.1f && rb.angularVelocity.magnitude < 0.1f){
+                    isRolling = false;
+                    CheckDiceFace();
+                }
+            }
+        }
     }
 
 
     void Update(){
-        
+        if(Keyboard.current != null && Keyboard.current.spaceKey.wasPressedThisFrame && !isRolling){
+            Debug.Log("space key was pressed and rolled the dice");
+            Roll();
+        }
+    }
+
+    void Roll(){
+        if(!isRolling){
+            rollTimer = 0f;
+            isRolling = true;
+            float rollForce = UnityEngine.Random.Range(minRollForce, maxRollForce);
+            float rollTorque = UnityEngine.Random.Range(minRollTorque, maxRollTorque);
+            Vector3 throwDirection = new Vector3(UnityEngine.Random.Range(-1f, 1f), 1f, UnityEngine.Random.Range(-1f, 1f)).normalized;
+            rb.AddForce(throwDirection * rollForce, ForceMode.Impulse);
+            rb.AddTorque(UnityEngine.Random.insideUnitSphere * rollTorque, ForceMode.Impulse);
+        }
+    }
+
+    void CheckDiceFace(){
+        Debug.Log("Dice has stopped :)");
+       
     }
 }
